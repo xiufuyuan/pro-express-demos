1ddabddd1fbcd807231d60bb074908cd57876e5a
xiufu.yuan <andlonger2016@gmail.com>
2016-06-11 20:29:02 +0800

    add handlers for router

--- ch5/routes/index.js
+++ ch5/routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var ping = function (req, res, next) {
+  console.log('ping');
+  return next();
+};
+
+var pong = function (req, res, next) {
+  console.log('pong');
+  return next();
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ping, pong, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
